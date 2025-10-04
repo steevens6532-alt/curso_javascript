9805044d4e28fb0ec93dc6dd945bd42e560eb47b
Steevens <steevens.6532@gmail.com>
2025-10-04 18:42:13 -0500

    testing de concatenacion

---
+++ js/05.js
@@ -0,0 +1,9 @@
+const nombre = "Steevens";
+const correo = "iloveu@gmail.com"
+
+
+//Concatenacion
+console.log(nombre + " " + correo);
+
+//Template Strings
+console.log(`Nombre: ${nombre} Correo: ${correo}`)
