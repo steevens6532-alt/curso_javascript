8f55dfe24e8d2bc930a92c289543e250d88309ce
Steevens <steevens.6532@gmail.com>
2025-10-05 20:34:07 -0500

    Probando el destructuring y añadiendo el archivo 7 al index

---
+++ js/07.js
@@ -0,0 +1,11 @@
+const empresa = {
+    nombre: "Empresa Peru",
+    direccion: "Av. El Peru",
+    ceo: "Eduardo Gonzales",
+    web: "www.empresa.pe",
+ }
+
+
+
+ const {nombre} = empresa;
+ console.log(nombre);
