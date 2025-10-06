43bc0546f290d1acd55952507e6277abb8aa2be5
Steevens <steevens.6532@gmail.com>
2025-10-06 11:40:37 -0500

    probando Destructing

---
+++ js/08.js
@@ -0,0 +1,11 @@
+const empresa = {
+    nombre: "Empresa Peru",
+    direccion: "Av. El Peru",
+    ceo: "Eduardo Gonzales",
+    web: "www.empresa.pe",
+ }
+
+ empresa.telefono = "5555555";
+
+
+ console.log(empresa);
