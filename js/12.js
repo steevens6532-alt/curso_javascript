2baaae52c12c33daa11939a4968b15fe727b9cf8
Steevens <steevens.6532@gmail.com>
2025-10-10 23:26:50 -0500

    Añadiendo nuevas funcionalidades

--- js/12.js
+++ js/12.js
@@ -10,13 +10,13 @@ console.log(sumar(10));
 
 
 //Expresion de la funcion
+console.log(sumar2(25));
 
 const sumar2 = function(parametro1){
     parametro1 = parametro1 + 1;
     return parametro1;
 }
 
-console.log(sumar2(25));
 
 //IIFE
 
